de02a0461f7866e2418c9340a2b6dc0aa9e20012
Dowan Kim <dowan1041@gmail.com>
2022-10-13 12:15:42 -0400

    next commit: project is done

---
+++ api/db/mongoose.js
@@ -0,0 +1,20 @@
+// This file will handel connection logic to the MongoDB Database
+
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TaskManager', { useNewUrlParser: true }).then(() => {
+    console.log("Connected to MongoDB successfully :)");
+}).catch((e) => {
+    console.log("Error while attempting to connect to MongoDB");
+    console.log(e);
+});
+
+// To prevent deprectation warnings (from MongoDB native driver)
+// mongoose.set('useCreateIndex', true);
+// mongoose.set('useFindAndModify', false);
+
+
+module.exports = {
+    mongoose
+};
